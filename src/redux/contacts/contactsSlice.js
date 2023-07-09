18fd3245460f030bcaabcbdceb402a630351f0f7
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-09 15:51:14 +0300

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -3,11 +3,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://64a87027dca581464b85bc9f.mockapi.io/api/v1',
+    baseUrl: 'https://64a87027dca581464b85bc9f.mockapi.io/api/v1/',
   }),
+  tagTypes: ['Contacts'],
   endpoints: builder => ({
     fetchContacts: builder.query({
-      query: () => `/contacts`,
+      query: () => '/contacts',
       providesTags: ['Contacts'],
     }),
     deleteContact: builder.mutation({
@@ -33,8 +34,8 @@ export const contactsApi = createApi({
 
 export const {
   useFetchContactsQuery,
-  useDeleteContactsMutation,
-  useCreateContactsMutation,
+  useDeleteContactMutation,
+  useCreateContactMutation,
 } = contactsApi;
 
 // !==========
