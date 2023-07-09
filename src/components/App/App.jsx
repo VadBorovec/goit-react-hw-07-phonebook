c4e14145c49543a8f75b77fc9a8d59870dc6f923
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-09 16:01:05 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,7 +11,7 @@ import { Container, Section } from 'components/ui';
 import { useFetchContactsQuery } from 'redux/contacts';
 import { Spinner, ContactForm, ContactList, CreateTodo } from 'components';
 
-export const App1 = () => {
+export const App = () => {
   // const contacts = useSelector(selectContact);
   const { data: contacts, isFetching } = useFetchContactsQuery();
   console.log(contacts);
